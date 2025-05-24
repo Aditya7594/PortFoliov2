656792d20b0e45edd9124daf8fc3d0c8b6683bba
Aditya7594 <73213462+Aditya990-jpg@users.noreply.github.com>
2025-05-24 15:19:32 +0530

    fix: update basePath and assetPrefix for GitHub Pages deployment (PortFoliov2)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from 'next'
 
+const repo = 'PortFoliov2'; // GitHub repo name
 const config: NextConfig = {
   output: 'export',
-  basePath: '/MyPortFolio',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   images: {
     unoptimized: true,
   },
